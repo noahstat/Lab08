e260df1f4bc73488db7ff5b641c1c2b6ac51e9cf
Noah Stasyszen <stasyszen@gmail.com>
2019-10-10 13:27:41 -0500

    fully implemented and tested

--- ContestantTest.java
+++ ContestantTest.java
@@ -1,3 +1,7 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
 /**
  * Lab 8
  *
@@ -12,73 +16,85 @@ public class ContestantTest
     /**
      * Tests that George's region is the expected value.
      */
+	@Test
     public void contestantGeorgeGetRegionTest() throws AssertException
     {
-    	// TODO: implement.
+    	assertEquals(Region.NORTH, Contestant.GEORGE.getRegion());
     }
     
     /**
      * Tests that Jill's region is the expected value.
      */
-    public void contestantJillGetRegionTest() throws AssertException
+	@Test
+	public void contestantJillGetRegionTest() throws AssertException
     {
-    	// TODO: implement.
+    	Assert.assertEquals(Region.SOUTH, Contestant.JILL.getRegion());
     }
 
     /**
      * Tests that Matthew's region is the expected value.
      */
+    @Test
     public void contestantMatthewGetRegionTest() throws AssertException
     {
-    	// TODO: implement.
+    	Assert.assertEquals(Region.EAST, Contestant.MATTHEW.getRegion());
     }
 
     /**
      * Tests that Betty's region is the expected value.
      */
+    @Test
     public void contestantBettyGetRegionTest() throws AssertException
     {
-    	// TODO: implement.
+    	Assert.assertEquals(Region.WEST, Contestant.BETTY.getRegion());
     }
 
     /**
      * Tests that George's hand choice is the expected value.
      */
+    @Test
     public void contestantGeorgeGetChoiceTest() throws AssertException
     {
-    	// TODO: implement.
+    	Assert.assertEquals(HandChoice.ROCK, Contestant.GEORGE.getChoice());
     }
     
     /**
      * Tests that Jill's hand choice is the expected value.
      */
 
+    @Test
     public void contestantJillGetChoiceTest() throws AssertException
     { 
-    	// TODO: implement.
+    	Assert.assertEquals(HandChoice.ROCK, Contestant.JILL.getChoice());
     }
 
     /**
      * Tests that Matthew's hand choice is the expected value.
      */
+    @Test
     public void contestantMatthewGetChoiceTest() throws AssertException
     {
-    	// TODO: implement.
+    	Assert.assertEquals(HandChoice.SCISSORS, Contestant.MATTHEW.getChoice());
     }
 
     /**
      * Tests that Betty's hand choice is the expected value.
      */
+    @Test
     public void contestantBettyGetChoiceTest() throws AssertException
     {
-    	// TODO: implement.
+    	Assert.assertEquals(HandChoice.PAPER, Contestant.BETTY.getChoice());
     }
 
     /**
      * Tests that Supercontestant.toString() returns the expected value for each contestant.
      */
+    @Test
     public void contestantToStringTest() throws AssertException
     {
-    	// TODO: implement.
+    	String expected = "betty: contestant from west throwing paper";
+    	String actual = Contestant.BETTY.toString();
+    	Assert.assertEquals(expected, actual);
     }
 }
+
